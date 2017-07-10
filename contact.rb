15f41919cd2c7f2c371d221c48e28035687ddcc8
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-10 13:31:14 -0400

    require and initialize connection to ActiveRecord

--- contact.rb
+++ contact.rb
@@ -1,3 +1,9 @@
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
+
 class Contact
 
 attr_reader :first_name, :last_name, :email
@@ -6,7 +12,6 @@ attr_writer :note
 @@contacts = []
 @@id = 1
 
-  # This method should initialize the contact's attributes
   def initialize (first_name, last_name, email, note)
     @first_name = first_name
     @last_name = last_name
@@ -16,21 +21,16 @@ attr_writer :note
     @@id += 1
   end
 
-  # This method should call the initializer,
-  # store the newly created contact, and then return it
   def self.create (first_name, last_name, email, note)
     new_contact = Contact.new(first_name, last_name, email, note)
     @@contacts<< new_contact
     return new_contact
   end
 
-  # This method should return all of the existing contacts
   def self.all
     return @@contacts
   end
 
-  # This method should accept an id as an argument
-  # and return the contact who has that id
   def self.find (id)
     @id = id
     @@contacts.each do |current_contact|
@@ -40,11 +40,6 @@ attr_writer :note
     end
   end
 
-
-  # This method should allow you to specify
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
   def update(attribute, new_value)
     case attribute
     when "first_name"
@@ -60,10 +55,6 @@ attr_writer :note
     end
   end
 
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
   def self.find_by (attribute, value)
     @@contacts.each do |current_contact|
       if attribute == "first_name" && current_contact.first_name == value
@@ -81,7 +72,6 @@ attr_writer :note
 
   end # CLOSE find_by
 
-  # This method should delete all of the contacts
   def self.delete_all
     @@contacts.clear
   end
@@ -90,20 +80,14 @@ attr_writer :note
     "#{first_name}""#{last_name}"
   end
 
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
+
   def delete
     @@contacts.delete(self)
   end
 
-  # Feel free to add other methods here, if you need them.
 
 betty = Contact.create('Betty', 'Maker',  'bettymakes@gmail.com', 'Loves Pokemon.')
 octo = Contact.create('Octo', 'Cat', 'octocat@github.com', 'Is it an octopus? Is it a cat?')
 madmax = Contact.create('Mad', 'Max', 'madmax@gail.com', 'crazy driver')
-
-
-# betty.delete
-# p @@contacts
 
 end
